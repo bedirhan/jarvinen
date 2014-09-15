cc370fcf0fdda90eac8aa9fad67c6bae56db2c1d
Gökhan ALKAN <cigalkan@gmail.com>
2014-09-15 14:44:05 +0300

    Update jarvinen.c

--- jarvinen.c
+++ jarvinen.c
@@ -4,29 +4,43 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <getopt.h>
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
 
+#define TAG_COUNT 32
 #define OVECCOUNT 30
 #define IDS_COUNT 1024
 
-void *apache_parser(void *param);
+// apt-get install  libxml2-dev
+// gcc jarvinen.c -o jarvinen -lpthread -lpcre -I/usr/include/libxml2 -lxml2
 
 sem_t g_sem;
 pcre *list[IDS_COUNT];
 char *idslist[IDS_COUNT];
+char *server_type[2] = {"apache", NULL};
+
+struct IDS_XML_NODE {
+        xmlChar *id;
+        xmlChar *rule;
+        xmlChar *description;
+        xmlChar *impact;
+        xmlChar *tag[TAG_COUNT];
+};
+
+void parse_node (xmlDocPtr doc, xmlNodePtr cur, struct IDS_XML_NODE *idsxml);
+static void parseDoc(char *docname, struct IDS_XML_NODE **idsxml);
+void *apache_parser(void *param);
 
 int main(int argc, char *argv[])
 {
     	pthread_t tid;
 
     	char *logfile;
-    	char *parser_line;
     	char *idsfile;
-    	char log_line[8092];
-    	char *ids_rules[1024];
-    	char ids_line[1024];
-	char *line;
+	char *line; 
+	char log_line[8092];
 	const char *error;
-    	int maxThread, erroffset, result, semval, i;
+    	int maxThread, erroffset, result, semval, check, i, k;
 
 	char *targ;
 	char *sarg;
@@ -38,7 +52,9 @@ int main(int argc, char *argv[])
 
     	FILE *log;
     	FILE *ids;
-    
+ 
+	struct IDS_XML_NODE *idsxml[IDS_COUNT];
+   
 	while ((ch = getopt(argc, argv, "l:x:s:t:")) != -1)
 		switch (ch) {
 			case 'l':
@@ -76,7 +92,35 @@ int main(int argc, char *argv[])
                 exit(EXIT_FAILURE);
 	}
 
+	check = 0;
+	for(i=0; server_type[i] != NULL; i++) {
+		if (strcmp(server_type[i], sarg) == 0) {
+			check = 1;
+			break;	
+		}
+	}
+
+	if (check == 0) {
+		fprintf(stderr, "\'%s\' is not valid server type. Please use one of them => ", sarg);
+		for (i=0; server_type[i] != NULL; i++)
+			printf("\'%s\' ", server_type[i]);
+		printf("\n");
+		exit(EXIT_FAILURE);
+	}
 	maxThread = (int) strtol(targ, NULL, 10);
+
+	parseDoc (idsfile, idsxml);
+	for(i=0; idsxml[i] != NULL; i++) {
+		idslist[i] = (char *)malloc(IDS_COUNT);
+		strncpy(idslist[i], (char *)idsxml[i]->rule, strlen((char *)idsxml[i]->rule)+1);
+		list[i] = pcre_compile( idslist[i], 0, &error, &erroffset, NULL );
+		if (list[i] == NULL) {
+                        fprintf(stderr, "PCRE compilation: %s failed at offset %d: %s\n", idslist[i], erroffset, error);
+                        exit(EXIT_FAILURE);
+                }
+        }
+	idslist[i] = NULL;
+        list[i] = NULL;
 
     	log = fopen(logfile, "r");
     	if (log == NULL) {
@@ -89,22 +133,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "\"%s\" Dosyasi Acilamadi !\n", idsfile);
 		exit(EXIT_FAILURE);
     	}
-    
-    	i = 0;
-    	while ( (fgets(ids_line, sizeof(ids_line), ids) ) != NULL) {
-		idslist[i] = (char *)malloc(IDS_COUNT);
-		strncpy(idslist[i], ids_line, strlen(ids_line)+1);
-		list[i] = pcre_compile( ids_line, 0, &error, &erroffset, NULL );
-                if (list[i] == NULL) {
-                	fprintf(stderr, "PCRE compilation: %s failed at offset %d: %s\n", ids_line, erroffset, error);
-                        exit(EXIT_FAILURE);
-                }
-		i = i + 1;
-    	}
-	idslist[i] = NULL;
-	list[i] = NULL;
-    	fclose(ids);
-    
+     
         if (sem_init(&g_sem, 0, maxThread) < 0) {
 		perror("sem_init");
 		exit(EXIT_FAILURE);
@@ -130,6 +159,16 @@ int main(int argc, char *argv[])
 	        pcre_free(list[i]);
 	}
 	
+	for (i=0; idsxml[i] != NULL; i++) {
+	    xmlFree(idsxml[i]->id);
+	    xmlFree(idsxml[i]->rule);
+	    xmlFree(idsxml[i]->description);
+	    xmlFree(idsxml[i]->impact);
+	    for(k=0; idsxml[i]->tag[k] != NULL; k++)
+		xmlFree(idsxml[i]->tag[k]);
+	    free(idsxml[i]);
+	}
+	
     	fclose(log);
 	    
 	pthread_exit(NULL);
@@ -144,7 +183,6 @@ void *apache_parser(void *param)
 	int ovector[OVECCOUNT];
 	char seperator[2] = " ";
 	char *token;
-	char **ids_rules;
 	char tmp_line[1024];
 
 	char *line = (char *)param;
@@ -171,3 +209,86 @@ void *apache_parser(void *param)
 
 	pthread_exit(NULL);
 }
+
+
+static void parseDoc(char *docname, struct IDS_XML_NODE **idsxml) {
+        xmlDocPtr doc;
+        xmlNodePtr cur;
+        int i;
+
+        doc = xmlParseFile(docname);
+        if (doc == NULL ) {
+                fprintf(stderr,"Document not parsed successfully. \n");
+                return;
+        }
+
+        cur = xmlDocGetRootElement(doc);
+        if (cur == NULL) {
+                fprintf(stderr,"empty document\n");
+                xmlFreeDoc(doc);
+                return;
+        }
+
+        if (xmlStrcmp(cur->name, (const xmlChar *) "filters")) {
+                fprintf(stderr,"document of the wrong type, root node != filters");
+                xmlFreeDoc(doc);
+                return;
+        }
+
+        i = 0;
+        cur = cur->xmlChildrenNode;
+        while (cur != NULL) {
+                if ((!xmlStrcmp(cur->name, (const xmlChar *)"filter"))){
+                        idsxml[i] = (struct IDS_XML_NODE *)malloc(sizeof(struct IDS_XML_NODE));
+                        parse_node(doc,cur, idsxml[i]);
+                        i = i + 1;
+                }
+                cur = cur->next;
+        }
+        idsxml[i] = NULL;
+
+        xmlFreeDoc(doc);
+
+        return;
+}
+
+
+void parse_node (xmlDocPtr doc, xmlNodePtr cur, struct IDS_XML_NODE *idsxml) {
+        xmlChar *id;
+        xmlChar *rule;
+        xmlChar *description;
+        xmlChar *impact;
+        xmlChar *tag[TAG_COUNT];
+        xmlNodePtr tmp_cur;
+        int count = 0;
+
+        cur = cur->xmlChildrenNode;
+        while (cur != NULL) {
+                if ((!xmlStrcmp(cur->name, (const xmlChar *)"id"))) {
+                        idsxml->id = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+                } else if ((!xmlStrcmp(cur->name, (const xmlChar *)"rule"))) {
+                        idsxml->rule = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+                } else if ((!xmlStrcmp(cur->name, (const xmlChar *)"description"))) {
+                        idsxml->description = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+                } else if ((!xmlStrcmp(cur->name, (const xmlChar *)"tags"))) {
+                        tmp_cur = cur;
+                        cur = cur->xmlChildrenNode;
+                        while(cur != NULL) {
+                                if ((!xmlStrcmp(cur->name, (const xmlChar *)"tag"))) {
+                                        idsxml->tag[count] = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+                                        count = count + 1;
+                                }
+                                cur = cur->next;
+                        }
+                        idsxml->tag[count] = NULL;
+                        cur = tmp_cur;
+                } else if ((!xmlStrcmp(cur->name, (const xmlChar *)"impact"))) {
+                        idsxml->impact = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
+                }
+
+                cur = cur->next;
+        }
+
+        return;
+}
+
